b5fc7d27948ecafb43fb8944bb3c92c160d7691e
Akshay Kumar <axykau@icloud.com>
2026-07-14 07:11:05 +0000

    Learn about variables

---
+++ JS-Class/01_variables.js
@@ -0,0 +1,8 @@
+const account_id = 249055; //const keyword cannot be changed
+let account_email = "akshay_249055@saitm.ac.in"; // prefer to use this way to decleare a variable
+var account_pass = "saitm@123"; // prefere to use let not var
+account_city = "bhiwani"; // this is not a professional way to declear a variable
+let account_state; // this method show the undefined in terminal we can update the value in future
+
+console.log(account_id); // normal way to print anything
+console.table([account_id,account_email,account_pass,account_city,account_state]); //this way helps to print the data in table formate
