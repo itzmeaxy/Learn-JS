46dc437c104ee10dbe4767423bf198fa80baf391
Akshay Kumar <axykau@icloud.com>
2026-07-14 12:34:25 +0000

    conversion operations

---
+++ JS-Class/03_conversion_operations.js
@@ -0,0 +1,13 @@
+let score = 7;
+// console.log(typeof score);
+
+let number_in_string = String(score);
+// console.log((number_in_string));
+// console.log(typeof number_in_string);
+
+let name = "akshay"
+// console.log(typeof name);
+
+let string_in_boolean = Boolean(name);
+// console.log(string_in_boolean);
+// console.log(typeof string_in_boolean);
