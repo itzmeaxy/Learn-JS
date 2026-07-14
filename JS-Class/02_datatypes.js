06f90c6ce0399519bcdec092f11c818c3afd5ffe
Akshay Kumar <axykau@icloud.com>
2026-07-14 09:49:46 +0000

    datatypes

---
+++ JS-Class/02_datatypes.js
@@ -0,0 +1,20 @@
+"use stricts"; // treat all JS code as newer version
+// alert("'-----") this syntax for brouser not for node js
+
+let name = "Aksahy" // string datatype
+let age = 20 // integer datatype
+let isloggegin = false // boolean datatype
+
+
+//              premitive datatype                
+//number :- 2 to power 53
+// bigint
+//boolean :- true/false
+//string :- "hi i am string" 
+//null :- standalone value   and it a object
+//undefined 
+//symbol :- unique
+
+
+//             non-prmitive datatype
+//object
