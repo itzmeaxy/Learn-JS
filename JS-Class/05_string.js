1411f0dc28cddd991279075902fd37b999cb030e
Akshay Kumar <axykau@icloud.com>
2026-07-16 06:13:54 +0000

    string

---
+++ JS-Class/05_string.js
@@ -0,0 +1,15 @@
+const name = "Akshay";// first way to declear a string
+const age = 20;// first way to declear a string
+
+// console.log(name + age);  it is not a good way 
+
+console.log(`My name is ${name}and my age is ${age}`) // it is a good way to 
+
+const game_name = new String("call of duty");// second way to declear a string
+console.log(game_name);// get the output in key values pair
+console.log(game_name[9])// get the output according the index numbe
+console.log(game_name.length);// this is the formate of using sting functions
+
+const str = new String("        hi i am akshay      ")
+ console.log(str.trim())
+ console.log(`hi it's my intro ${str}`)
